3482597190bf6bc1cfbb8d8899689ab05df26838
OorjitVedamurthy123 <63858193+OorjitVedamurthy123@users.noreply.github.com>
2021-03-31 17:28:54 +0530

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,95 +1,96 @@
 import React, { Component } from 'react';
-import { View, StyleSheet, Text, FlatList,TouchableOpacity, StackList} from 'react-native';
+import { View, StyleSheet, Text, FlatList,TouchableOpacity,Image } from 'react-native';
 import { ListItem } from 'react-native-elements'
 import firebase from 'firebase';
 import db from '../config'
 
-export default class HomeScreen extends Component{
-    constructor(){
-        super()
-        this.state={
-            allRequests:[]
-        }
-        this.requestRef = null
-    }
-    getRequestedExchangeItems=()=>{
-        this.requestRef = db.collection("exchange_requests")
-        .onSnapshot((snapshot)=>{
-            var allRequests = snapshot.docs.map(document => document.data());
-            console.log(allRequests)
-            this.setState({
-                allRequests : allRequests
-            })
-        })
-    }
 
-    componentDidMount(){
-        this.getRequestedExchangeItems()
+export default class BookDonateScreen extends Component{
+  constructor(){
+    super()
+    this.state = {
+      allRequests : []
     }
+  this.requestRef= null
+  }
 
-    componentWillUnmount(){
-        this.requestRef()
-    }
+   getRequestedExchangeItems=()=>{
+    this.requestRef = db.collection("exchange_requests")
+    .onSnapshot((snapshot)=>{
+      var allRequests = snapshot.docs.map((doc) => doc.data())
+      this.setState({
+        allRequests : allRequests
+      });
+    })
+  }
 
-    keyExtractor = (item, index) => index.toString()
+  
 
-    renderItem=({item, i}) => {
-        console.log(item.item_name)
-        return(
-            <ListItem
-                key = {i}
-                title={item.item_name}
-                subtitle={item.description}
-                titleStyle={{color:'black', fontWeight:'bold'}}
-                rightElement={
-                    <TouchableOpacity style={styles.button}>
-                        <Text style={{color:'#ffff'}}>Exchange</Text>
-                    </TouchableOpacity>
-                }
-                bottomDivider
-                />
-        )
-    }
-    render(){
-        return(
-            <View style={{flex:1}}>
-              {
-                this.state.allRequests.length === 0
-                ?(
-                  <View style={styles.subContainer}>
-                    <Text style={{ fontSize: 20}}>List of all the exchange items</Text>
-                  </View>
-                )
-                :(
-                  <FlatList
-                    keyExtractor={this.keyExtractor}
-                    data={this.state.allRequests}
-                    renderItem={this.renderItem}
-                  />
-                )
-              }
-            </View>
-        )
-      }
-    }
+  componentWillUnmount(){
+    this.requestRef = null;
+  }
+
+  renderItem = ( {item, index} ) =>{
+    console.log(item.item_name, index)
+    return (
+      <ListItem key={index} bottomDivider>
+        <ListItem.Content>
+          <ListItem.Title>{item.item_name}</ListItem.Title>
+          <ListItem.Subtitle>{item.description}</ListItem.Subtitle>
+          
+        </ListItem.Content>
+        <TouchableOpacity style={{backgroundColor:"red", padding:7}}>
+          <Text>Exchange</Text>
+        </TouchableOpacity>
+      </ListItem>
+    );
+  };
+  componentDidMount(){
+    this.getRequestedExchangeItems()
+  }
+
+  render(){
+    return(
+      <View style={{flex:1}}>
+        <View style={{flex:1}}>
+          {
+            this.state.allRequests.length === 0
+            ?(
+              <View style={styles.subContainer}>
+                <Text style={{ fontSize: 20}}>List Of All Exchange Items</Text>
+              </View>
+            )
+            :(
+              <FlatList
+                keyExtractor={(item, index)=> index.toString()}
+                data={this.state.allRequests}
+                renderItem={this.renderItem}
+              />
+            )
+          }
+        </View>
+      </View>
+    )
+  }
+}
 
 const styles = StyleSheet.create({
-    subContainer:{
-      flex:1,
-      fontSize: 20,
-      justifyContent:'center',
-      alignItems:'center'
-    },
-    button:{
-      width:100,
-      height:30,
-      justifyContent:'center',
-      alignItems:'center',
-      backgroundColor:"#ff5722",
-      shadowColor: "#000",
-      shadowOffset: {
-         width: 0,
-         height: 8
-       }
-    }
-  })
+  subContainer:{
+    flex:1,
+    fontSize: 20,
+    justifyContent:'center',
+    alignItems:'center'
+  },
+  button:{
+    width:100,
+    height:30,
+    justifyContent:'center',
+    alignItems:'center',
+    backgroundColor:"#ff5722",
+    shadowColor: "#000",
+    shadowOffset: {
+       width: 0,
+       height: 8
+     }
+  }
+})
